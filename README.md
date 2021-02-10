7c7f9dbe823e0369c8d3045e78ddef3d43cdd4ef
grace-stytch <grace@stytch.com>
2021-02-10 12:04:09 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# stytch-node-magic-links
+
+##### 1. Set ENV vars
+Set your project ID and secret in the `.env` file.
+
+##### 2. Run the Server
+
+Run `node server.js`
+
+##### 3. Login
+
+Visit `http://localhost:4567` and login with your email.
+Then check for the Stytch email and click the sign in button.
+You should be signed in!
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const url = require('url');
+const stytch = require("stytch")
+
+const app = express();
+const port = process.env.PORT;
+const path = `http://localhost:${port}`
+const magicLinkUrl = `${path}/authenticate`
+
+// bodyParser allows us to access the body of the post request
+app.use(bodyParser.urlencoded({ extended: true }));
+// defines the directory where the static assets are so images & css render correctly
+app.use(express.static('public'));
+// set app to use ejs so we can use html templates
+app.set('view engine', 'ejs')
+
+// define the stytch client using your stytch project id & secret
+// use stytch.envs.live if you want to hit the live api
+const client = new stytch.Client({
+        project_id: process.env.STYTCH_PROJECT_ID,
+        secret: process.env.STYTCH_SECRET,
+        env: stytch.envs.test,
+    }
+);
+
+// define the homepage route
+app.get("/", (req, res) => {
+    res.render('loginOrSignUp');
+});
+
+// takes the email entered on the homepage and hits the stytch
+// loginOrCreateUser endpoint to send the user a magic link
+app.post('/login_or_create_user', function (req, res) {
+    const params = stytch.LoginOrCreateRequest = {
+        email: req.body.email,
+        login_magic_link_url: magicLinkUrl,
+        signup_magic_link_url: magicLinkUrl,
+    };
+    client.loginOrCreate(params)
+        .then(
+            // on success, render the emailSent page
+            res.render('emailSent')
+        )
+        .catch(err => {
+            // on failure, log the error then render the homepage
+            console.log(err)
+            res.render('loginOrSignUp')
+        });
+})
+
+// this is the endpoint the link in the magic link hits takes the token from the
+// link's query params and hits the stytch authenticate endpoint to verify the token is valid
+app.get('/authenticate', function (req, res) {
+    const queryObject = url.parse(req.url,true).query;
+    client.authenticateMagicLink(queryObject.token, null)
+        .then(
+            // on success render the logged in view
+            res.render('loggedIn')
+        )
+        .catch(err => {
+            // on failure, log the error then render the homepage
+            console.log(err)
+            res.render('loginOrSignUp')
+        });
+})
+
+// handles the logout endpoint
+app.get('/logout', function (req, res) {
+    res.render('loggedOut');
+})
+
+// run the server
+app.listen(port, () => {
+    console.log(`Listening to requests on ${path}`);
+});
